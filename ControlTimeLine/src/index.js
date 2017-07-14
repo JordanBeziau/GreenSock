4137345a2ee4d161585b94b7e1b7a4fbfb7d2840
JordanBeziau <jordanbeziau@gmail.com>
2017-07-14 19:02:39 +0200

    Control Timeline with methods

--- ControlTimeLine/src/index.js
+++ ControlTimeLine/src/index.js
@@ -2,9 +2,49 @@ import { TimelineLite, CSSPlugin } from "gsap";
 
 document.addEventListener("DOMContentLoaded", () => {
   const Timeline = new TimelineLite();
-  const all = document.querySelectorAll("header > *");
+  const all = Array.from(document.querySelectorAll("header > *:not(ul)")),
+    lists = Array.from(document.querySelectorAll("header ul > li"));
+
+  lists.forEach(list => all.push(list));
 
   all.forEach(element => {
-    Timeline.from(element, 0.3, { autoAlpha: 0, y: -50 }, "-=.15");
+    Timeline.from(element, 0.3, { autoAlpha: 0, y: -50 }, "-=.10");
   });
+  Timeline.pause();
+
+  document
+    .getElementById("tlPlay")
+    .addEventListener("click", () => Timeline.play());
+
+  document
+    .getElementById("tlPause")
+    .addEventListener("click", () => Timeline.pause());
+
+  document
+    .getElementById("tlResume")
+    .addEventListener("click", () => Timeline.resume());
+
+  document
+    .getElementById("tlReverse")
+    .addEventListener("click", () => Timeline.reverse());
+
+  document
+    .getElementById("tlSpeedUp")
+    .addEventListener("click", () => Timeline.timeScale(2));
+
+  document
+    .getElementById("tlSlow")
+    .addEventListener("click", () => Timeline.timeScale(0.5));
+
+  document
+    .getElementById("tlSeek")
+    .addEventListener("click", () => Timeline.seek(1));
+
+  document
+    .getElementById("tlGoTo")
+    .addEventListener("click", () => Timeline.progress(0.5)); // 0 start, 1 end
+
+  document
+    .getElementById("tlRestart")
+    .addEventListener("click", () => Timeline.restart());
 });
