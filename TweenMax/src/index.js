42648c28812dd682511f30ab53d1cff662b8ed0a
JordanBeziau <jordanbeziau@gmail.com>
2017-07-15 18:39:23 +0200

    Using TweenMax to master animations...

--- TweenMax/src/index.js
+++ TweenMax/src/index.js
@@ -1,30 +1,126 @@
-import { TweenLite, CSSPlugin } from "gsap";
+import { TweenMax } from "gsap";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const header = document.querySelector("h2"),
-    img = document.querySelector("img"),
-    h1 = document.querySelector("h1"),
-    p = document.querySelector("p");
+  const Timeline = new TimelineLite({ paused: true });
 
-  TweenLite.from(h1, 1, { autoAlpha: 0, y: 50 });
-  TweenLite.from(p, 1, { autoAlpha: 0, y: 50, delay: 0.5 });
-  TweenLite.fromTo(
-    img,
-    1,
-    { autoAlpha: 0, x: "-50%" },
-    { autoAlpha: 1, x: "50%", delay: 1 }
-  );
-  TweenLite.fromTo(
-    header,
-    1,
-    { autoAlpha: 0, x: "50%" },
-    { autoAlpha: 1, x: "-50%", delay: 1 }
-  );
-  TweenLite.to(img, 0.5, { x: "0%", delay: 2 });
-  TweenLite.to(header, 0.5, { x: "0%", delay: 2 });
+  const boxHTML = document.getElementById("boxHTML"),
+    boxSVG = document.getElementById("boxSVG"),
+    htmlDOT = document.getElementById("htmlDOT"),
+    circ = document.querySelector(".circ"),
+    circStroke = document.querySelector(".circStroke"),
+    rect = document.querySelector("svg .rect"),
+    hello = document.querySelector(".hello"),
+    title = document.querySelector(".hello h1");
 
-  TweenLite.to(h1, 1, { autoAlpha: 0, x: -300, delay: 4 });
-  TweenLite.to(p, 1, { autoAlpha: 0, x: 300, delay: 4 });
-  TweenLite.to(img, 1, { autoAlpha: 0, x: -300, delay: 4 });
-  TweenLite.to(header, 1, { autoAlpha: 0, x: 300, delay: 4 });
+  Timeline.to(boxHTML, 0.7, {
+    x: "100%",
+    y: "100%",
+    ease: Power2.easeInOut
+  })
+    .to(boxSVG, 0.7, { x: "100%", y: "100%", ease: Power2.easeInOut })
+    .addPause()
+    .to(boxHTML, 0.7, { top: "50%", left: "50%", ease: Power2.easeInOut })
+    .to(boxSVG, 0.7, { top: "50%", left: "50%", ease: Power2.easeInOut })
+    .addPause()
+    .to(boxHTML, 0.7, {
+      left: "50%",
+      top: "50%",
+      x: "-50%",
+      y: "-50%",
+      ease: Power2.easeInOut
+    })
+    .to(htmlDOT, 0.7, { top: 0, left: 0, ease: Power2.easeInOut })
+    .addPause()
+    .to(boxSVG, 0.7, {
+      left: "50%",
+      top: "50%",
+      x: "-50%",
+      y: "-50%",
+      ease: Power2.easeInOut
+    })
+    .to(circ, 0.7, { attr: { cx: 0, cy: 0 }, ease: Power2.easeInOut })
+    .addPause()
+    .to(boxHTML, 0.7, {
+      rotation: 90,
+      transformOrigin: "100% 100%",
+      ease: Power2.easeInOut
+    })
+    .addPause()
+    .to(boxSVG, 0.7, {
+      rotation: 90,
+      transformOrigin: "100% 100%",
+      ease: Power2.easeInOut
+    })
+    .addPause()
+    .to(boxHTML, 0.7, { xPercent: -100, rotation: 0, ease: Power2.easeInOut })
+    .addPause()
+    .to(boxSVG, 0.7, { x: "0%", rotation: 0, ease: Power2.easeInOut })
+    .addPause()
+    .to(htmlDOT, 0.7, { top: "50%", left: "50%", ease: Power2.easeInOut })
+    .to(circ, 0.7, { attr: { cx: 100, cy: 100 }, ease: Power2.easeInOut })
+    .addPause()
+    .to([boxHTML, boxSVG], 0.7, {
+      rotation: 720,
+      transformOrigin: "50% 50%",
+      ease: Power2.easeInOut
+    })
+    .addPause()
+    .to(boxHTML, 0.7, {
+      rotationX: -180,
+      transformOrigin: "0% 50%",
+      ease: Power2.easeInOut
+    })
+    .to(boxSVG, 0.7, {
+      rotationY: -180,
+      transformOrigin: "100% 50%",
+      ease: Power2.easeInOut
+    })
+    .addPause()
+    .to(boxHTML, 0.7, {
+      rotationX: -360,
+      ease: Power2.easeInOut
+    })
+    .to(boxSVG, 0.7, {
+      rotationY: -360,
+      ease: Power2.easeInOut
+    })
+    .addPause()
+    .to([boxHTML, boxSVG], 0.7, {
+      top: "50%",
+      left: "50%",
+      x: "-50%",
+      y: "-50%",
+      ease: Power2.easeInOut
+    })
+    .addPause()
+    .to([boxSVG, boxHTML], 1.5, {
+      rotation: 1440,
+      transformOrigin: "50% 50%",
+      ease: Power4.easeIn
+    })
+    .to([rect, boxHTML], 0.2, { autoAlpha: 0 }, "-=0.2")
+    .fromTo(
+      circStroke,
+      0.5,
+      { scale: 0.5, transformOrigin: "50% 50%" },
+      {
+        autoAlpha: 1,
+        scale: 1,
+        transformOrigin: "50% 50%",
+        ease: Power2.easeIn
+      },
+      "-=0.3"
+    )
+    .to(circStroke, 0.5, { scale: 0.5, transformOrigin: "50% 50%" })
+    .to(hello, 0.4, { autoAlpha: 1, ease: Power2.easeIn }, "-=0.5")
+    .to(title, 0.4, { autoAlpha: 1, scale: 2, ease: Power2.easeIn }, "-=0.5")
+    .addPause();
+
+  document
+    .getElementById("next")
+    .addEventListener("click", () => Timeline.play());
+
+  document
+    .getElementById("prev")
+    .addEventListener("click", () => Timeline.reverse());
 });
